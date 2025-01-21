f199a029504fb1ddf1ddc6152f112f8f7c975a72
nurhossainfarid <faahsan1516@gmail.com>
2025-01-21 23:37:44 +0600

    add validation in academic semester form

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,9 +1,10 @@
 import { Button } from "antd";
 import PHForm from "../../../components/form/PHForm";
-import PHInput from "../../../components/form/PHInput";
 import PHSelect from "../../../components/form/PHSelect";
 import { semesterOptions } from "../../../constants/semester";
 import { monthOptions } from "../../../constants/global";
+import { academicSemesterSchema } from "../../../schemas/academicMangement.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const currentYear = new Date().getFullYear();
 const yearOptions = [0, 1, 2].map((number) => ({
@@ -11,13 +12,19 @@ const yearOptions = [0, 1, 2].map((number) => ({
   label: (currentYear + number).toString(),
 }));
 
+
+
 const CreateAcademicSemester = () => {
+  
   const onSubmit = (data: any) => {
     console.log(data);
   };
   return (
     <div>
-      <PHForm onSubmit={onSubmit}>
+      <PHForm
+        onSubmit={onSubmit}
+        resolver={zodResolver(academicSemesterSchema)}
+      >
         <PHSelect name="name" label="Name" options={semesterOptions} />
         <PHSelect name="year" label="Year" options={yearOptions} />
         <PHSelect
