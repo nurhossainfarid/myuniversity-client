96fb5815312641707353fe53140b8295286767d7
nurhossainfarid <faahsan1516@gmail.com>
2025-01-22 09:37:30 +0600

    handle all types of error when create academic semester

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,13 +1,9 @@
-import { useGetAcademicSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
+import { useGetAcademicSemestersQuery } from "../../../redux/features/admin/academicManagement";
 
 const AcademicSemester = () => {
-    const {data} = useGetAcademicSemestersQuery(undefined);
-    console.log(data);
-    return (
-        <div>
-            Academic Semester
-        </div>
-    );
+  const { data } = useGetAcademicSemestersQuery(undefined);
+  console.log(data);
+  return <div>Academic Semester</div>;
 };
 
 export default AcademicSemester;
--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,41 +1,70 @@
-import { Button } from "antd";
+import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHForm from "../../../components/form/PHForm";
+import { Button, Col, Flex } from "antd";
 import PHSelect from "../../../components/form/PHSelect";
 import { semesterOptions } from "../../../constants/semester";
 import { monthOptions } from "../../../constants/global";
-import { academicSemesterSchema } from "../../../schemas/academicMangement.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { toast } from "sonner";
+import { TResponse } from "../../../types/global";
+import { academicSemesterSchema } from "../../../schemas/academicMangement.schema";
+import { useAddAcademicSemesterMutation } from "../../../redux/features/admin/academicManagement";
 
 const currentYear = new Date().getFullYear();
-const yearOptions = [0, 1, 2].map((number) => ({
-  value: (currentYear + number).toString(),
-  label: (currentYear + number).toString(),
+const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
+  value: String(currentYear + number),
+  label: String(currentYear + number),
 }));
 
-
-
 const CreateAcademicSemester = () => {
-  
-  const onSubmit = (data: any) => {
-    console.log(data);
+  const [addAcademicSemester] = useAddAcademicSemesterMutation();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Creating...");
+
+    const name = semesterOptions[Number(data?.name) - 1]?.label;
+
+    const semesterData = {
+      name,
+      code: data.name,
+      year: data.year,
+      startMonth: data.startMonth,
+      endMonth: data.endMonth,
+    };
+
+    try {
+      const res = (await addAcademicSemester(semesterData)) as TResponse;
+      console.log(res);
+      if (res?.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success("Semester created", { id: toastId });
+      }
+    } catch (err) {
+      toast.error("Something went wrong", { id: toastId });
+    }
   };
+
   return (
-    <div>
-      <PHForm
-        onSubmit={onSubmit}
-        resolver={zodResolver(academicSemesterSchema)}
-      >
-        <PHSelect name="name" label="Name" options={semesterOptions} />
-        <PHSelect name="year" label="Year" options={yearOptions} />
-        <PHSelect
-          name="startMonth"
-          label="Start Month"
-          options={monthOptions}
-        />
-        <PHSelect name="endMonth" label="End Month" options={monthOptions} />
-        <Button htmlType="submit">Submit</Button>
-      </PHForm>
-    </div>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm
+          onSubmit={onSubmit}
+          resolver={zodResolver(academicSemesterSchema)}
+        >
+          <PHSelect label="Name" name="name" options={semesterOptions} />
+          <PHSelect label="Year" name="year" options={yearOptions} />
+          <PHSelect
+            label="Start Month"
+            name="startMonth"
+            options={monthOptions}
+          />
+          <PHSelect label="End Month" name="endMonth" options={monthOptions} />
+
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
