3bdf32b85ee73eca7ee86a5038e6b23bfd02e223
nurhossainfarid <faahsan1516@gmail.com>
2025-01-22 20:07:56 +0600

    show academic faculties data in table

--- src/pages/admin/academicManagement/AcademicFaculty.tsx
+++ src/pages/admin/academicManagement/AcademicFaculty.tsx
@@ -1,10 +1,34 @@
+import { Table, TableColumnsType } from "antd";
+import { TAcademicFaculty } from "../../../types";
+import { useGetAcademicFacultiesQuery } from "../../../redux/features/admin/academicManagement";
+
+export type TTableData = Pick<TAcademicFaculty, "name">;
 
 const AcademicFaculty = () => {
-    return (
-        <div>
-           Academic Faculty 
-        </div>
-    );
+  const { data: semesterData } = useGetAcademicFacultiesQuery(undefined);
+
+  const data = semesterData?.data?.map(
+    ({ _id, name }) => ({
+      key: _id,
+      name,
+    })
+  );
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      showSorterTooltip: { target: "full-header" },
+    },
+  ];
+
+  return (
+    <Table<TTableData>
+      columns={columns}
+      dataSource={data}
+      showSorterTooltip={{ target: "sorter-icon" }}
+    />
+  );
 };
 
 export default AcademicFaculty;
