1ab488bce5cfac8e20754da0e764f2d068e892f6
nurhossainfarid <faahsan1516@gmail.com>
2025-01-24 16:54:04 +0600

    handle role authentication

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,6 +5,7 @@ import { adminPaths } from "./admin.routes";
 import { facultyPaths } from "./faculty.routes";
 import { studentPaths } from "./student.routes";
 import Login from "../pages/Login";
+import ProtectedRoute from "../components/layout/ProtectedRoute";
 // import ProtectedRoute from "../components/layout/ProtectedRoute";
 
 const routes = createBrowserRouter([
@@ -12,28 +13,31 @@ const routes = createBrowserRouter([
     path: "/",
     element: <App />,
   },
-  // {
-  //   path: "/admin",
-  //   element: (
-  //     <ProtectedRoute>
-  //       <App />
-  //     </ProtectedRoute>
-  //   ),
-  //   children: routesGenerator(adminPaths),
-  // },
   {
     path: "/admin",
-    element: <App />,
+    element: (
+      <ProtectedRoute role="admin">
+        <App />
+      </ProtectedRoute>
+    ),
     children: routesGenerator(adminPaths),
   },
   {
     path: "/faculty",
-    element: <App />,
+    element: (
+      <ProtectedRoute role="faculty">
+        <App />
+      </ProtectedRoute>
+    ),
     children: routesGenerator(facultyPaths),
   },
   {
     path: "/student",
-    element: <App />,
+    element: (
+      <ProtectedRoute role="student">
+        <App />
+      </ProtectedRoute>
+    ),
     children: routesGenerator(studentPaths),
   },
   {
